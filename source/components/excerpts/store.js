394b11a2dfdf36fa847a3849a2622fffdc8f318d
djembuka <djembuka@gmail.com>
2021-04-28 16:30:40 +0300

    Mobile fixes

---
+++ source/components/excerpts/store.js
@@ -0,0 +1,31 @@
+window.excerptStore = {
+  blocks: [
+    {
+      head: 'Упрощенная выписка 2',
+      btnUrl: '/components/excerpts/getExcerpt.json',
+      title: 'Упрощенная выписка &quot;СРО ААС&quot; 8',
+      date: '20 марта 2021',
+      state: 'excerpt',
+      files: {
+        pdf: {
+          url: '/pages/excerpt1/',
+          size: '8.07 Мб',
+        },
+        sig: {
+          url: '/pages/excerpt2/',
+          size: '18.07 Мб',
+        },
+      },
+    },
+    {
+      head: 'Упрощенная выписка 3',
+      btnUrl: '/components/excerpts/getExcerpt.json',
+      state: 'empty',
+    },
+    {
+      head: 'Упрощенная выписка 4',
+      btnUrl: '/components/excerpts/getExcerpt.json',
+      state: 'loading',
+    },
+  ],
+};
