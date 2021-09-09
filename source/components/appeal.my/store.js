9bb31ee0424fd487e680ebf156bcb5b8eddcb042
djembuka <djembuka@gmail.com>
2021-09-09 11:56:32 +0300

    My appeals

---
+++ source/components/appeal.my/store.js
@@ -0,0 +1,58 @@
+window.selectTypeData = {
+  options: [
+    {
+      label: 'Все',
+      code: '',
+    },
+    {
+      label: 'Изменение в реестре',
+      code: 'prop1',
+    },
+    {
+      label: 'Жалоба',
+      code: 'prop2',
+    },
+    {
+      label: 'Новость',
+      code: 'prop3',
+    },
+  ],
+  selected: {
+    label: 'Все',
+    code: '',
+  },
+};
+window.selectStatusData = {
+  options: [
+    {
+      label: 'Все',
+      code: '',
+    },
+    {
+      label: 'Новые',
+      code: 'prop1',
+    },
+    {
+      label: 'Отклоненные',
+      code: 'prop2',
+    },
+    {
+      label: 'В работе',
+      code: 'prop3',
+    },
+    {
+      label: 'Выполненые',
+      code: 'prop4',
+    },
+  ],
+  selected: {
+    label: 'Все',
+    code: '',
+  },
+};
+
+window.myScriptURL = {
+  getTable: '/components/appeal.my/table.json',
+};
+
+window.inboxSrcPath = '/template/images/';
