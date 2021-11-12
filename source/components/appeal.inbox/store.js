c50ff2de54f4b9659aa3a83081c1da3aa07a4cdc
djembuka <djembuka@gmail.com>
2021-11-12 18:10:27 +0300

    Appeal inbox

--- source/components/appeal.inbox/store.js
+++ source/components/appeal.inbox/store.js
@@ -1,4 +1,67 @@
+window.appealIndexStore = {
+  timeout: 2000,
+  paths: {
+    getTable: '/components/appeal.inbox/table.json',
+    getNewNum: '/components/appeal.inbox/newNum.json',
+    src: '/template/images/',
+  },
+  filter: {
+    controls: [
+      {
+        code: 'id',
+        count: 1,
+        type: 'fio',
+        label: 'Номер (ID)',
+      },
+      {
+        code: 'object',
+        count: 3,
+        type: 'fio',
+        label: 'Объект изменений',
+      },
+      {
+        code: 'author',
+        count: 3,
+        type: 'fio',
+        label: 'Автор (ФИО, ОРНЗ)',
+      },
+      {
+        code: 'status',
+        type: 'select',
+        label: 'Статус обращения',
+        options: [
+          {
+            label: 'Все',
+            code: '',
+          },
+          {
+            label: 'Новые',
+            code: 'prop1',
+          },
+          {
+            label: 'Отклоненные',
+            code: 'prop2',
+          },
+          {
+            label: 'В работе',
+            code: 'prop3',
+          },
+          {
+            label: 'Выполненые',
+            code: 'prop4',
+          },
+        ],
+        selected: {
+          label: 'Все',
+          code: '',
+        },
+      },
+    ],
+  },
+};
+
 window.selectData = {
+  name: 'status',
   options: [
     {
       label: 'Все',
@@ -26,10 +89,3 @@ window.selectData = {
     code: '',
   },
 };
-
-window.inboxScriptURL = {
-  getTable: '/components/appeal.inbox/table.json',
-  getNewNum: '/components/appeal.inbox/newNum.json',
-};
-
-window.inboxSrcPath = '/template/images/';
