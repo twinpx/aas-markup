dbb278bd3a62eaa1b252bd52493b710e107c390c
djembuka <djembuka@gmail.com>
2022-02-16 18:07:13 +0300

    Notifications

--- source/components/notifications/script.js
+++ source/components/notifications/script.js
@@ -5,62 +5,78 @@ window.addEventListener('load', () => {
         .querySelector('.b-header')
         .classList.contains('ph-block--animated')
     ) {
-      //top
-      document
-        .getElementById('topNotification')
-        .classList.add('top-notification--show');
+      clearInterval(intervalId);
+      //nc
+      document.querySelectorAll('.nc').forEach((nc) => {
+        const type = nc.getAttribute('data-type'); //top, count, content, modal
+        const url = nc.getAttribute('data-url');
+
+        (async () => {
+          try {
+            let response = await fetch(`${url}?type=${type}`);
+            let result = await response.json();
+            switch (type) {
+              case 'top':
+                nc.innerHTML = `<div class="notification notification--top">
+                  <div class="notification__content container-fluid">${result.response.CONTENT}<a class="btn btn-md" href="${result.response.LINK}" style="margin-left: 35px;">Нужен текст ссылки</a></div>
+                  <svg class="notification__clear" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="16" height="16" viewBox="0 0 16 16">
+                    <defs>
+                      <clipPath id="clip-path">
+                        <rect width="16" height="16" fill="none"></rect>
+                      </clipPath>
+                    </defs>
+                    <g clip-path="url(#clip-path)">
+                      <path d="M9.2,7.986l6.548-6.548A.842.842,0,1,0,14.562.247L8.013,6.8,1.465.247A.842.842,0,0,0,.274,1.438L6.822,7.986.247,14.562a.842.842,0,1,0,1.191,1.191L8.013,9.178l6.548,6.549a.842.842,0,0,0,1.191-1.191Z" transform="translate(0 0)" fill="#fff"></path>
+                    </g>
+                  </svg>
+                </div>`;
+                break;
+              case 'content':
+                nc.innerHTML = `<div class="notification notification--inside">
+                  <div class="notification__content">${result.response.CONTENT}<a class="notification__clear btn btn-md" href="" style="margin-left: 35px;">Понятно</a></div>
+                </div>`;
+                break;
+              case 'count':
+                nc.innerHTML = `<span class="notification notification--count"><span>${result.response.CONTENT}</span></span>`;
+                break;
+            }
+            //events
+            setTimeout(() => {
+              if (type === 'top') {
+                document
+                  .querySelector('body')
+                  .classList.add('body--notifications');
+              }
+              nc.querySelector('.notification').classList.add(
+                'notification--show'
+              );
+
+              //clear
+              nc.querySelector('.notification__clear').addEventListener(
+                'click',
+                (e) => {
+                  e.preventDefault();
+                  //hide
+                  nc.classList.remove('notification--show');
+                  if (type === 'top') {
+                    document
+                      .querySelector('body')
+                      .classList.remove('body--notifications');
+                  }
+                }
+              );
+            }, 100);
+          } catch (err) {
+            throw err;
+          }
+        })();
+      });
       //notifications
       document.querySelectorAll('.notification').forEach((n) => {
         n.classList.add('notification--show');
       });
       //popup
       $('#notModal').modal('show');
-
-      clearInterval(intervalId);
     }
   }, 100);
-
-  //clear
-  document.querySelectorAll('.notification__clear').forEach((clear) => {
-    clear.addEventListener('click', function (e) {
-      e.preventDefault();
-      //hide
-      e.target.closest('.notification').classList.remove('notification--show');
-      if (
-        e.target
-          .closest('.notification')
-          .classList.contains('notification--top')
-      ) {
-        document.querySelector('body').classList.remove('body--notifications');
-      }
-    });
-  });
-
-  //top
-  document
-    .querySelector('#topNotificationClear')
-    .addEventListener('click', async function (e) {
-      e.preventDefault();
-
-      //fetch
-      let formData = new FormData();
-      formData.append('id', id);
-      let response = await fetch('', {
-        method: 'POST',
-        headers: {
-          Authorization: 'token',
-        },
-        body: formData,
-      });
-
-      let result = await response.json();
-
-      if ((result.STATUS = 'Y')) {
-        //hide
-        document
-          .querySelector('#topNotification')
-          .classList.remove('top-notification--show');
-        document.querySelector('body').classList.remove('body--notifications');
-      }
-    });
 });
