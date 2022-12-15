eaff37b17fa1f339debadeab6a8be2f05f003335
djembuka <djembuka@gmail.com>
2022-12-15 14:34:45 +0300

    Course detail

--- source/components/study.courses.detail/script.js
+++ source/components/study.courses.detail/script.js
@@ -2,6 +2,42 @@
   'use strict';
 
   $(function () {
+    //icon copy
+    const copyToClipboard = (str, elem) => {
+      if (navigator && navigator.clipboard && navigator.clipboard.writeText) {
+        str = str.trim();
+        navigator.clipboard.writeText(str);
+        if (elem) {
+          let span = document.createElement('span');
+          span.classList.add('b-copy-icon__note');
+          span.innerText = 'Скопировано в буфер';
+          elem.querySelector('.b-copy-icon').appendChild(span);
+          setTimeout(() => {
+            span.classList.add('b-copy-icon__note--show');
+          }, 0);
+          setTimeout(() => {
+            span.classList.remove('b-copy-icon__note--show');
+          }, 1000);
+          setTimeout(() => {
+            span.remove();
+          }, 1500);
+        }
+        return;
+      }
+      return Promise.reject('The Clipboard API is not available.');
+    };
+
+    if (window.matchMedia('(min-width: 768px)').matches) {
+      //table
+      document
+        .querySelectorAll('.b-study-courses-detail table.table td')
+        .forEach((td) => {
+          td.addEventListener('click', () => {
+            copyToClipboard(td.textContent, td);
+          });
+        });
+    }
+
     //tabs
     document
       .querySelectorAll('.b-study-courses-detail')
